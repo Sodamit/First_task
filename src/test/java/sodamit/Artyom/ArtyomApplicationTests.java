c134fd3df48ff5aab8b87be3f928d46e819b1c8a
Jimmy Black <135708408+Sodamit@users.noreply.github.com>
2023-06-06 17:33:59 +0400

    Add files via upload

---
+++ src/test/java/sodamit/Artyom/ArtyomApplicationTests.java
@@ -0,0 +1,13 @@
+package sodamit.Artyom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtyomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
